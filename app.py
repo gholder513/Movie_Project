dde7121308873d170b664f7a4ea57ad1bd98c99c
gholder513 <67515375+gholder513@users.noreply.github.com>
2022-09-11 13:13:14 -0400

    Added CSS and Movie Picker Render Template

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
-from views import views
+from flask import Flask, render_template, redirect, url_for
+#from views import views
 
 app = Flask(__name__)
 
-app.register_blueprint(views, url_prefix="/home")
+#app.register_blueprint(views, url_prefix="/home")
+
+@app.route("/Home")
+def home():
+    return render_template("home.html")
+
+@app.route("/MoviePicker")
+def movie_picker():
+   return render_template("moviepicker.html")
+
+@app.route("/")
+def home2():
+    return render_template("home.html")
 
 if __name__== '__main__':
     app.run(debug=True, port=8000)
--- views.py
+++ views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+from app import app
 
 views = Blueprint(__name__, "views")
 
-@views.route("/")
+@app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
